f77bf500919e22f3644019fb012dc70506535baf
Ching Casanova Tapia <casanovatapia@gmail.com>
2016-07-21 16:04:10 -0400

    separate js file

--- js/validator.js
+++ js/validator.js
@@ -1,1 +1,13 @@
+var arr = ["advertainment", "big data", "clickbait", "disruptor", "freemium",
+"gamification", "growth hack", "hyperlocal", "ideation", "innovator", "millenial",
+"netiquette", "omnichannel", "real-time", "snackable", "storyscaping", "solomo", "synergy",
+"transparency", "viral", "wantrapreneur", "wearable"];
 
+function validate() {
+    var input = document.getElementById('myform').elements.item(0).value;
+    if (arr.indexOf(input) == -1 ) {
+        window.alert("Sorry, that wasn't a buzzword. Try again.");
+    } else {
+        window.alert("Yay, that was a buzzword. You rock.")
+    }
+}
